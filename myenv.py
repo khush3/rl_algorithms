62d9b714a0818073cbe519a0cbfc6e286ed6640f
Khush <agrawalkhush2000@gmail.com>
2019-06-18 12:27:41 +0530

    added multiple enemy feature to env and solved discrete env.

--- myenv.py
+++ myenv.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pickle
 from matplotlib import style
 import time
+import numpy as np
 
 style.use("ggplot")
 
@@ -38,7 +39,6 @@ class Blob():
                 self.move(x=1, y=-1)
 
         else:
-            print(choice)
             if choice == 0:
                 self.move(x=0, y=1)
             elif choice == 1:
@@ -51,20 +51,16 @@ class Blob():
 
     def move(self, x=-100, y=-100):
 
-        # If no value for x, move randomly
         if x == -100:
             self.x += np.random.randint(-1, 2)
         else:
             self.x += x
 
-        # If no value for y, move randomly
         if y == -100:
             self.y += np.random.randint(-1, 2)
         else:
             self.y += y
 
-
-        # If we are out of bounds, fix!
         if self.x < 0:
             self.x = 0
         elif self.x > self.size-1:
@@ -79,24 +75,35 @@ class ENVIRONMENT():
 
 
 
-    def __init__(self, player_number=1, enemy_numer=1, food_number=1, size = 10, DIAGONAL = False):
+    def __init__(self, num_player=1, num_enemy=1, num_food=1, size = 10, diagonal = False):
         self.size = size
-        self.diagonal = DIAGONAL
+        self.diagonal = diagonal
+        self.num_enemy = num_enemy
+        self.num_food = num_food
         self.player = Blob(size)
-        self.enemy = Blob(size)
-        self.food = Blob(size)
+        self.enemy = [Blob() for _ in range(self.num_enemy)]
+        self.food = [Blob() for _ in range(self.num_food)]
         self.reward = 0
         self.colors = {1: (255, 0, 0),
          2: (0, 255, 0),
          3: (0, 0, 255)}
+        self.px,self.py = self.player.x,self.player.y
+        self.ex,self.ey = [self.enemy[iter].x for iter in range(self.num_enemy)], [self.enemy[iter].y for iter in range(self.num_enemy)]
+        self.fx,self.fy = [self.food[iter].x for iter in range(self.num_food)], [self.food[iter].y for iter in range(self.num_food)]
 
-    def reset(self):
-        self.player = Blob(self.size)
-        self.enemy = Blob(self.size)
-        self.food = Blob(self.size)
+
+    def startover(self, newpos=False):
+
+        self.player.x, self.player.y = self.px, self.py
+        for iter in range(self.num_enemy):
+            self.enemy[iter].x, self.enemy[iter].y = self.ex[iter], self.ey[iter]
+        for iter in range(self.num_food):
+            self.food[iter].x, self.food[iter].y = self.fx[iter], self.fy[iter]
+        if newpos == True:
+            self.player = Blob(self.size)
         self.reward = 0
 
-        return (self.player.x, self.player.y), self.reward
+        return (self.player.x, self.player.y), self.reward, False
 
     def step(self, action):
 
@@ -106,10 +113,10 @@ class ENVIRONMENT():
 
     def calculate_reward(self):
 
-        if self.player.x == self.enemy.x and self.player.y == self.enemy.y:
+        if self.player.x in [self.enemy[iter].x for iter in range(self.num_enemy)] and self.player.y in [self.enemy[iter].y for iter in range(self.num_enemy)]:
             return -100, True
 
-        if self.player.x == self.food.x and self.player.y == self.food.y:
+        if self.player.x in [self.food[iter].x for iter in range(self.num_food)] and self.player.y in [self.food[iter].y for iter in range(self.num_food)]:
             return 100, True
 
         else:
@@ -119,9 +126,11 @@ class ENVIRONMENT():
     def render(self):
 
         env = np.zeros((self.size, self.size, 3), dtype=np.uint8)
-        env[self.food.x][self.food.y] = self.colors[2]
+        for iter in range(self.num_food):
+            env[self.food[iter].x][self.food[iter].y] = self.colors[2]
+        for iter in range(self.num_enemy):
+            env[self.enemy[iter].x][self.enemy[iter].y] = self.colors[3]
         env[self.player.x][self.player.y] = self.colors[1]
-        env[self.enemy.x][self.enemy.y] = self.colors[3]
         img = Image.fromarray(env, 'RGB')
         img = img.resize((300, 300))
         cv2.imshow("image", np.array(img))
